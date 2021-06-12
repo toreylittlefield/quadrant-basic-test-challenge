a5aeb011c7e3aece847c20d5466d8921fb6e6c41
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-12 23:22:01 +0800

    test - formatted prettier extension on save

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,7 @@
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello World!
-      </header>
+      <header className="App-header">Hello World!</header>
     </div>
   );
 }
