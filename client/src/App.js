516b92e6e4b60d714b7f4658a1e05684bae670cd
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-14 22:55:58 +0800

    create FileUploadSVG component from Figma in icons

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+import FileUploadSVG from "./assets/icons";
+
 function App() {
   return (
     <div className="App">
+      <FileUploadSVG />
       <header className="App-header">Hello World!</header>
     </div>
   );
