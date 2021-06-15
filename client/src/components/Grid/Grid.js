924f7fb69219274d5660f81367ad59345452e05f
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-15 16:24:18 +0800

    add general grid

---
+++ client/src/components/Grid/Grid.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Grid = styled.div`
+  display: grid;
+  height: 100ev;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 0.2fr 1.5fr 1.2fr 0.8fr;
+`;
+
+export default Grid;
