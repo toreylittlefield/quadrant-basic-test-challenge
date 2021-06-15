2bb7f8a62eaff7007dc940585d5b7a28cc7a8df9
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-15 17:49:52 +0800

    starting grid in MainPage

---
+++ client/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Grid } from "./Grid/Grid";
+export { default as TopNav } from "./Navs/TopNav";
+export { default as MainNav } from "./Navs/MainNav";
+export { default as SubNav } from "./Navs/SubNav";
--- client/src/screens/MainPage.js
+++ client/src/screens/MainPage.js
@@ -1,22 +1,13 @@
 import React from "react";
 
-import {
-  GlobeIconSVG,
-  FileUploadSVG,
-  LibraryIconSVG,
-  GridButtonIconSVG,
-} from "assets/icons";
-
-import CroppedBootCutJeans from "assets/images/Cropped Boot Cut Jeans 1 1.png";
+import { Grid, TopNav, MainNav, SubNav } from "components";
 
 const MainPage = () => (
-  <div>
-    <GlobeIconSVG />
-    <FileUploadSVG />
-    <LibraryIconSVG />
-    <GridButtonIconSVG />
-    <img alt="Cropped Boot Cut Jeans" src={CroppedBootCutJeans} />
-  </div>
+  <Grid>
+    <TopNav>Top Nav</TopNav>
+    <MainNav>Main Nav</MainNav>
+    <SubNav>Sub Nav</SubNav>
+  </Grid>
 );
 
 export default MainPage;
