8fbd441128ebee9caa1f819a478c1c5051eeda4c
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-15 11:41:11 +0800

    add comments in globalstyles.js

--- client/src/ui/globalstyles.js
+++ client/src/ui/globalstyles.js
@@ -9,12 +9,15 @@ export const Global = createGlobalStyle`
     box-sizing: border-box;
   }
   :root {
+      // mobile
       font-size: ${responsive(24)};
 
+      // tablet
       @media (min-width: 768px) {
         font-size: ${responsive(18)};
       }
-
+      
+      // desktop
       @media (min-width: 1024px) {
         font-size: ${responsive(16)};
       }
