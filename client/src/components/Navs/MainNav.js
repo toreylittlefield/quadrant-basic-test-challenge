1982bc48a672fc569710b7acb950056fc8cf271b
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-15 17:48:58 +0800

    update MainNav component matching figma

--- client/src/components/Navs/MainNav.js
+++ client/src/components/Navs/MainNav.js
@@ -2,26 +2,27 @@ import styled from "styled-components";
 import React from "react";
 
 const SideNav = styled.nav`
-  background-color: green;
-  grid-row: 1 / span all;
-  grid-column: 1 / span 1;
+  grid-row: 1;
+  grid-column: 1 / span 2;
+  display: grid;
+  grid-template-columns: repeat(12, [col-start] 1fr);
+`;
+
+const MainSideNav = styled.nav`
+  grid-column: 1 / 4;
+  background-color: blanchedalmond;
   text-align: center;
-  z-index: 1;
-  overflow: hidden;
-  height: 100%;
 `;
 
 const SubNav = styled.nav`
-  background-color: blanchedalmond;
+  background-color: green;
+  grid-column: 4 / span all;
   text-align: center;
-  width: 75%;
-  margin-left: 25%;
-  height: 100%;
 `;
 
 const MainNav = () => (
   <SideNav>
-    SideNav - MainNav
+    <MainSideNav>SideNav - MainNav</MainSideNav>
     <SubNav>Expanded Nav</SubNav>
   </SideNav>
 );
