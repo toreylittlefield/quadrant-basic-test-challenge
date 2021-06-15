42027173966874608d69193d3b6a609ba36d07cf
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-15 23:08:04 +0800

    add Theme component & update props to children

--- client/src/screens/MainPage.js
+++ client/src/screens/MainPage.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { Theme } from "ui/Theme";
 
 import { Grid, TopNav, MainNav, SubNav } from "components";
 
 const MainPage = () => (
-  <Grid>
-    <TopNav>Top Nav</TopNav>
-    <MainNav>Main Nav</MainNav>
-    <SubNav>Sub Nav</SubNav>
-  </Grid>
+  <Theme>
+    <Grid>
+      <TopNav>Option Attributes</TopNav>
+      <MainNav bgColorDark bgColorLight dividersColor>
+        Main Nav
+      </MainNav>
+      <SubNav bgColorLight>Sub Nav</SubNav>
+    </Grid>
+  </Theme>
 );
 
 export default MainPage;
