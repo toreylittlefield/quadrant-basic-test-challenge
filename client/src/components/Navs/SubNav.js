b9678b6407a94051924f1a821a5b5480c3369980
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-15 23:07:06 +0800

    add theme color type to bg-color

--- client/src/components/Navs/SubNav.js
+++ client/src/components/Navs/SubNav.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
 const SubNav = styled.nav`
-  background-color: aqua;
+  background-color: ${(props) => props.theme.bgColorLight};
   grid-row: 2 / span 2;
-  grid-column: 3 / span all;
+  grid-column: 3 / all;
   text-align: center;
+  margin-right: 50px;
 `;
 
 export default SubNav;
