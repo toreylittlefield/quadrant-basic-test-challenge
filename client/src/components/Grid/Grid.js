6231c83127efb1147594891122688161b7a46409
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-15 23:06:07 +0800

    update Grid component

--- client/src/components/Grid/Grid.js
+++ client/src/components/Grid/Grid.js
@@ -2,10 +2,9 @@ import styled from "styled-components";
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(12, [col-start] 1fr);
-  grid-auto-rows: 200px;
+  grid-template-columns: repeat(12, [col] 1fr);
+  grid-template-rows: repeat(12, [row] 50px);
   column-gap: 20px;
-  padding: 0 45px 0 45px;
 `;
 
 export default Grid;
