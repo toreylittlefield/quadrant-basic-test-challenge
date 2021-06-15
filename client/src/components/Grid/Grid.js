8964729877e69d257af6c3c600ad4e5967b2201c
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-15 17:48:35 +0800

    update Grid to standard 12 cols, padding 45

--- client/src/components/Grid/Grid.js
+++ client/src/components/Grid/Grid.js
@@ -2,9 +2,10 @@ import styled from "styled-components";
 
 const Grid = styled.div`
   display: grid;
-  height: 100ev;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: 0.2fr 1.5fr 1.2fr 0.8fr;
+  grid-template-columns: repeat(12, [col-start] 1fr);
+  grid-auto-rows: 200px;
+  column-gap: 20px;
+  padding: 0 45px 0 45px;
 `;
 
 export default Grid;
