a618e41cb545cdfe9a11034ed1f0c0c53122f8db
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-15 23:07:28 +0800

    update alignment in TopNav component

--- client/src/components/Navs/TopNav.js
+++ client/src/components/Navs/TopNav.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 const TopNav = styled.nav`
-  background-color: red;
   grid-row: 1 / span 1;
   grid-column: 3 / span all;
-  text-align: center;
+  text-align: left;
+  align-self: center;
 `;
 
 export default TopNav;
