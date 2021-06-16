db11067ef224e5e9bde4f5a284e2ebfef21a0366
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-16 23:34:16 +0800

    change grid-template-rows: 80px;

--- client/src/components/Grid/Grid.js
+++ client/src/components/Grid/Grid.js
@@ -4,8 +4,18 @@ const Grid = styled.div`
   min-height: 900px;
   display: grid;
   grid-template-columns: repeat(12, [col] 1fr);
-  grid-template-rows: repeat(12, [row] 50px);
+  /* grid-template-rows: repeat(12, [row] 50px); */
+  /* grid-template-rows: repeat(12, [row] 1fr); */
+  grid-template-rows: 80px;
   column-gap: 20px;
+  // tablet
+  @media (max-width: 768px) {
+    /* column-count: 6; */
+  }
+  // desktop
+  @media (max-width: 451px) {
+    /* column-count: 1; */
+  }
 `;
 
 export default Grid;
