eb9916a8deccd79a146ba0911b8a7cdadbacc269
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-16 18:04:29 +0800

    moved LinkNav To Own Component

---
+++ client/src/components/Navs/LinkNav.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const LinkNav = styled.a`
+  cursor: pointer;
+  text-decoration: none;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    background-color: ${(props) =>
+      props.Dark
+        ? props.theme.bgColorLightHover
+        : props.theme.bgColorDarkHover};
+  }
+  &:active {
+    background-color: ${(props) =>
+      props.Dark ? props.theme.bgColorLightActive : props.theme.bgColorLight};
+    animation: ${(props) => props.theme.animateClick};
+  }
+  ${(props) => props.theme.clicked}
+`;
+
+export default LinkNav;
