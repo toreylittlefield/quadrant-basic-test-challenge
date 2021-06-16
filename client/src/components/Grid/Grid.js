d5679e0c655920578bff887871606ab2b5d488c0
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-16 10:10:38 +0800

    add min height to grid of 900px

--- client/src/components/Grid/Grid.js
+++ client/src/components/Grid/Grid.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 const Grid = styled.div`
+  min-height: 900px;
   display: grid;
   grid-template-columns: repeat(12, [col] 1fr);
   grid-template-rows: repeat(12, [row] 50px);
