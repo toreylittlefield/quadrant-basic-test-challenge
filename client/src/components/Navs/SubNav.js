2db385b6f8b3e9cca5def110b31fb8515d46e4ae
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-16 23:35:48 +0800

    remove grid props & add flex box as parent

--- client/src/components/Navs/SubNav.js
+++ client/src/components/Navs/SubNav.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 
 const SubNav = styled.nav`
   background-color: ${(props) => props.theme.bgColorLight};
-  grid-row: 2 / span 2;
-  grid-column: 3 / all;
   text-align: center;
-  margin-right: 50px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
 `;
 
 export default SubNav;
