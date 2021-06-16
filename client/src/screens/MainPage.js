ea5ae23ceb005d8c9bd903f810d76e9e0a3e946c
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-16 23:36:51 +0800

    import image & container & dummy SubNav children

--- client/src/screens/MainPage.js
+++ client/src/screens/MainPage.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { Theme } from "ui/Theme";
 
-import { Grid, TopNav, MainNav, SubNav } from "components";
+import {
+  Grid,
+  TopNav,
+  MainNav,
+  SubNav,
+  MainImage,
+  Container,
+} from "components";
 
+/**
+ * This is the Main Page "Screen / View" with all parent components rendered here
+ * @returns MainPage component
+ */
 const MainPage = () => (
   <Theme>
     <Grid>
@@ -10,7 +21,18 @@ const MainPage = () => (
       <MainNav bgColorDark bgColorDarkHover bgColorLight dividersColor>
         Main Nav
       </MainNav>
-      <SubNav bgColorLight>Sub Nav</SubNav>
+      <Container>
+        <MainImage />
+        <SubNav bgColorLight>
+          Sub Nav
+          <ul>
+            <li>Classification</li>
+            <li>Commercial</li>
+            <li>Notes</li>
+            <li>Links</li>
+          </ul>
+        </SubNav>
+      </Container>
     </Grid>
   </Theme>
 );
