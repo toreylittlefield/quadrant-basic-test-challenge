8e31eb3510e32db27f26cb0ddbf0670a51418df0
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-16 18:28:31 +0800

    add comments to TopNav

--- client/src/components/Navs/TopNav.js
+++ client/src/components/Navs/TopNav.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 
+/**
+ * Component for the top of the ink-list in the side bar
+ */
 const TopNav = styled.nav`
   grid-row: 1 / span 1;
   grid-column: 3 / span all;
