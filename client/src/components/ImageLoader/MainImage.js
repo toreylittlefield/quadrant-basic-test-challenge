397b301ea0c890eb55d2e228d6abf29ee029cf13
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-17 23:56:33 +0800

    clean up

--- client/src/components/ImageLoader/MainImage.js
+++ client/src/components/ImageLoader/MainImage.js
@@ -4,17 +4,8 @@ import MainImageSource from "assets/images/Cropped Boot Cut Jeans 1 1.png";
 
 const MainImageStyle = styled.img`
   max-height: 475px;
+  max-width: 100%;
   margin-bottom: 25px;
-  margin-right: 25px;
-  /* grid-column: 6; */
-  // tablet
-  /* @media (min-width: 768px) {
-    grid-column: 4;
-  }
-  // desktop
-  @media (min-width: 1024px) {
-    grid-column: 3;
-  } */
 `;
 
 const MainImage = () => <MainImageStyle src={`${MainImageSource}`} />;
