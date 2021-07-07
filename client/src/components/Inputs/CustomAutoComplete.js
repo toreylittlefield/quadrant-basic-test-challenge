90f81320a53c07ee1abec5fed6ab31391ae91283
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-07-07 22:23:27 +0800

    CustomAutoComplete, add styles, add enter event,

--- client/src/components/Inputs/CustomAutoComplete.js
+++ client/src/components/Inputs/CustomAutoComplete.js
@@ -4,12 +4,20 @@ import TextField from "@material-ui/core/TextField";
 import Autocomplete, {
   createFilterOptions,
 } from "@material-ui/lab/Autocomplete";
+import { makeStyles } from "@material-ui/core";
 
 const filter = createFilterOptions({ ignoreCase: true, trim: true });
 const defaultInputValues = {
   newValue: "",
   inputValue: "",
 };
+
+const useStyles = makeStyles({
+  root: {
+    width: "clamp(325px,375px,500px)",
+    padding: "5px 0px 5px 0px",
+  },
+});
 
 const CustomAutoComplete = ({
   setFormValues = () => {},
@@ -18,31 +26,38 @@ const CustomAutoComplete = ({
   id = "",
   displayLabel = "",
 }) => {
+  const classes = useStyles();
   const [value, setValue] = React.useState(defaultInputValues);
   const { inputValue } = value;
 
+  // passed setState from props
+  const handleSetNewFormValues = (newValue) => {
+    setFormValues((prev) => ({
+      ...prev,
+      [stateKey]: [...prev[stateKey], newValue],
+    }));
+  };
+
+  // local state
+  const handleSetNewValueInput = (newValue) => {
+    setValue(() => ({
+      newValue: newValue,
+      inputValue: newValue,
+    }));
+  };
+
   return (
     <Autocomplete
+      classes={{
+        root: classes.root,
+      }}
       id={id}
-      noOptionsText="No Options Found"
       value={inputValue}
       onChange={(event, val) => {
-        if (typeof val === "string") {
-          setValue((prev) => ({
-            ...prev,
-            inputValue: val,
-          }));
-        } else if (val && val.newValue) {
+        if (val && val.newValue) {
           // Create a new value from the user input
-          setValue((prev) => ({
-            ...prev,
-            newValue: val.newValue,
-            inputValue: val.newValue,
-          }));
-          setFormValues((prev) => ({
-            ...prev,
-            [stateKey]: [...prev[stateKey], val.newValue],
-          }));
+          handleSetNewValueInput(val.newValue);
+          handleSetNewFormValues(val.newValue);
         } else {
           setValue((prev) => ({
             ...prev,
@@ -52,18 +67,13 @@ const CustomAutoComplete = ({
       }}
       filterOptions={(options, params) => {
         const filtered = filter(options, params);
-
         // Suggest the creation of a new value
-        if (
-          params.inputValue !== "" &&
-          !options.find((option) => option === params.inputValue)
-        ) {
+        if (params.inputValue !== "" && filtered.length === 0) {
           filtered.push({
             newValue: params.inputValue,
             addOption: `Add "${params.inputValue}"`,
           });
         }
-
         return filtered;
       }}
       selectOnFocus
@@ -71,22 +81,38 @@ const CustomAutoComplete = ({
       handleHomeEndKeys
       options={inputDataArray}
       getOptionLabel={(option) => {
-        // Value selected with enter, right from the input
-        if (typeof option === "string") {
-          return option;
-        }
         // Add "xxx" opion created dynamically
         if (option.newValue) {
           return option.newValue;
         }
-        // Regular option
         return option;
       }}
       renderOption={(option) => (option.addOption ? option.addOption : option)}
-      style={{ width: 300 }}
       freeSolo
       renderInput={(params) => (
-        <TextField {...params} label={displayLabel} variant="outlined" />
+        <TextField
+          {...params}
+          label={displayLabel}
+          onKeyDown={(event) => {
+            // Suggest the creation of a new value
+            // if (params.inputValue !== "" && filtered.length === 0) {
+            //   filtered.push({
+            //     newValue: params.inputValue,
+            //     addOption: `Add "${params.inputValue}"`,
+            //   });
+            // }
+            if (event.key === "Enter") {
+              const filtered = filter(inputDataArray, {
+                inputValue: event.target.value,
+                getOptionLabel: (option) => option,
+              });
+              if (filtered.length === 0) {
+                handleSetNewValueInput(event.target.value);
+                handleSetNewFormValues(event.target.value);
+              }
+            }
+          }}
+        />
       )}
     />
   );
