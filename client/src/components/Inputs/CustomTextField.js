b3618da1efe5be10b89360c3ed5bd709c34c499c
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-07-07 22:24:19 +0800

    CustomTextField add margin & padding

--- client/src/components/Inputs/CustomTextField.js
+++ client/src/components/Inputs/CustomTextField.js
@@ -5,7 +5,7 @@ import { TextField } from "@material-ui/core";
 const CustomTextField = ({ setFormValues = () => {}, name = "" }) => (
   <TextField
     id="standard-name"
-    style={{ width: 375 }}
+    style={{ width: 375, marginRight: 8, padding: "5px 5px 5px 0px" }}
     label="Name"
     value={name}
     onChange={(e) =>
